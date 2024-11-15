bbfb2da91429504bb98558706ea14e9861ab0d3b
EBRAT HOSSEN <105649400+Ebrat222@users.noreply.github.com>
2024-11-15 12:17:48 +0600

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ module.exports = {
     useProxy: false, // Set to true if you want to run with proxies
     minDelay: 3, // random delay for keepalive packet send
     maxDelay: 10, // random delay for keepalive packet send
-    restartDelay: 5, // Delay in seconds before restarting the process
-    accountDelay: 500, // Custom delay in seconds before processing the next account
+    restartDelay: 100, // Delay in seconds before restarting the process
+    accountDelay: 5, // Custom delay in seconds before processing the next account
 };
